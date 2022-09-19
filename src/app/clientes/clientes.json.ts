fe358b618428387dec4688b5ae9df5d89d6bb614
LuisPumajulca <pumajulca@hotmail.com>
2022-09-19 18:57:51 -0500

    se agregan configuraciones

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,20 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Luis', apellido: 'Pumajulca', email:'pumajulca@hotmail.com', createAt:'1996-07-19'},
+    {id: 2, nombre: 'Pedro', apellido: 'Hernandez', email:'hernandez@hotmail.com', createAt:'1996-03-11'},
+    {id: 3, nombre: 'Adrian', apellido: 'Perez', email:'perez@hotmail.com', createAt:'1996-03-20'},
+    {id: 4, nombre: 'Elmer', apellido: 'Casas', email:'casas@hotmail.com', createAt:'1991-07-01'},
+    {id: 5, nombre: 'Juan', apellido: 'Rojas', email:'rojas@hotmail.com', createAt:'2000-05-10'},
+    {id: 6, nombre: 'Elena', apellido: 'Reejas', email:'reejas@hotmail.com', createAt:'1993-02-31'},
+    {id: 7, nombre: 'Maria', apellido: 'Bonifacio', email:'bonifacio@hotmail.com', createAt:'1999-07-20'},
+    {id: 8, nombre: 'Joiselyn', apellido: 'Cauti', email:'cauti@hotmail.com', createAt:'1991-05-30'},
+    {id: 9, nombre: 'Katy', apellido: 'Castillo', email:'castillo@hotmail.com', createAt:'1986-05-17'},
+    {id: 10, nombre: 'Shen', apellido: 'Roman', email:'roman@hotmail.com', createAt:'1980-01-13'},
+    {id: 11, nombre: 'Cap', apellido: 'Rodriguez', email:'rodriguez@hotmail.com', createAt:'1990-05-12'},
+    {id: 12, nombre: 'Karol', apellido: 'Alejos', email:'alejos@hotmail.com', createAt:'1997-12-30'},
+    {id: 13, nombre: 'Mariana', apellido: 'Caceres', email:'caceres@hotmail.com', createAt:'2001-01-12'},
+    {id: 14, nombre: 'Yerard', apellido: 'Benites', email:'benites@hotmail.com', createAt:'1993-07-10'},
+    {id: 15, nombre: 'Fernando', apellido: 'Rojas', email:'rojas@hotmail.com', createAt:'1992-08-08'},
+    {id: 16, nombre: 'Ezly', apellido: 'Ochoa', email:'ocho@hotmail.com', createAt:'1997-03-15'}
+  ];
