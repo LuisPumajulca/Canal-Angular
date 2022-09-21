9b7fec8b0423fc3503c001f76f62ee6d9647e419
LuisPumajulca <pumajulca@hotmail.com>
2022-09-21 13:35:13 -0500

    21/09/22: manejo de errores cap 66

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -34,16 +34,16 @@ export class FormComponent implements OnInit {
     this.clienteService.create(this.cliente)
       .subscribe(cliente => {
          this.router.navigate(['/clientes'])
-         Swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
+         Swal.fire('Nuevo cliente', `El cliente ${cliente.nombre} ha sido creado con éxito`, 'success')
       }
     );
   }
 
   update(): void{
     this.clienteService.update(this.cliente)
-      .subscribe(cliente => {
+      .subscribe(json => {
         this.router.navigate(['/clientes'])
-        Swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito`, 'success')
+        Swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}`, 'success')
       }
     );
   }
