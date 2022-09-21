c529fa665d1e6b3d0cf24b49a52c5d303a3df1b7
LuisPumajulca <pumajulca@hotmail.com>
2022-09-21 17:09:07 -0500

    validando form desde backend cap 72-73

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -13,6 +13,8 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente()
   public titulo:string = "Crear Cliente"
 
+  public errores: string[];
+
   constructor(private clienteService: ClienteService, 
     private router: Router, 
     private activatedRoute: ActivatedRoute) { }
@@ -35,6 +37,11 @@ export class FormComponent implements OnInit {
       .subscribe(cliente => {
          this.router.navigate(['/clientes'])
          Swal.fire('Nuevo cliente', `El cliente ${cliente.nombre} ha sido creado con éxito`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
       }
     );
   }
@@ -44,6 +51,11 @@ export class FormComponent implements OnInit {
       .subscribe(json => {
         this.router.navigate(['/clientes'])
         Swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
       }
     );
   }
